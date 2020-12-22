011860837f108f9bfcc7813e9c0cdd366b2d1ae4
obaidaattaee <obaataii@gmail.com>
2020-12-22 18:49:18 +0200

    asdasd

--- app/Http/Controllers/Site/PaymentCallbackController.php
+++ app/Http/Controllers/Site/PaymentCallbackController.php
@@ -2,9 +2,17 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
+use App\Models\Advertisement;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class PaymentCallbackController extends Controller{
     public function index (){
-        dd(request()->session()->get('advertisement')) ;
-    }
+        dd(request());
+        $advertisement = Advertisement::findOrfail(request()->session()->get('advertisement')['advertisement_id']);
+        $advertisement->active = 1 ;
+        $advertisement->verified = 1 ;
+        $advertisement->save();
+        Alert::alert('تم اضافة اعلانك بنجاح') ;
+        return redirect(route('main'));
+   }
 }
